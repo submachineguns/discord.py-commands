2aec1c7e1ba75e4d2b47e50cf7712c5d74163717
twitterr <73202450+twitterr@users.noreply.github.com>
2021-03-17 18:47:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -78,16 +78,18 @@ async def unjail(ctx):
 
 
 @commands.command(aliases=['bitcoin'])
-    async def btc(self, ctx):
-        r = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR')
-        r = r.json()
-        usd = r['USD']
-        eur = r['EUR']
-        embed = discord.Embed(color=(0xb699ff))
-        embed.description=f'**{str(usd)}$ USD**'
-        embed.set_author(name='Bitcoin', icon_url='https://media.discordapp.net/attachments/806971294795956254/808381684012810310/600.png?width=480&height=480')
-        embed.set_footer(text='updated bitcoin prices to this hour')
-        await ctx.send(embed=embed)
+async def btc(self, ctx):
+	r = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR')
+	r = r.json()
+	usd = r['USD']
+	eur = r['EUR']
+	embed = discord.Embed(color=(0xb699ff))
+	embed.description=f'**{str(usd)}$ USD**'
+	embed.set_author(name='Bitcoin', icon_url='https://media.discordapp.net/attachments/806971294795956254/808381684012810310/600.png?width=480&height=480')
+	embed.set_footer(text='updated bitcoin prices to this hour')
+	await ctx.send(embed=embed)
+	    
+
 
 @client.command(brief='Restarts the bot.')
 @commands.has_permissions(ban_members=True)
