ed80b38df17ae0ffe3cc29901c931b7695707372
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-07-07 02:25:04 -0400

    Update reactions.py

--- reactions.py
+++ reactions.py
@@ -5,7 +5,6 @@ from discord import Embed
 from discord.ext.commands import Cog
 from discord.ext.commands import command, has_permissions
 
-from ..db import db
 
 # Here are all the number emotes.
 # 0⃣ 1️⃣ 2⃣ 3⃣ 4⃣ 5⃣ 6⃣ 7⃣ 8⃣ 9⃣
@@ -20,75 +19,6 @@ class Reactions(Cog):
 		self.polls = []
 		self.giveaways = []
 
-
-	@command(name="createpoll", aliases=["mkpoll"])
-	@has_permissions(manage_guild=True)
-	async def create_poll(self, ctx, hours: int, question: str, *options):
-		if len(options) > 10:
-			await ctx.send("You can only supply a maximum of 10 options.")
-
-		else:
-			embed = Embed(title="Poll",
-						  description=question,
-						  colour=ctx.author.colour,
-						  timestamp=datetime.utcnow())
-
-			fields = [("Options", "\n".join([f"{numbers[idx]} {option}" for idx, option in enumerate(options)]), False),
-					  ("Instructions", "React to cast a vote!", False)]
-
-			for name, value, inline in fields:
-				embed.add_field(name=name, value=value, inline=inline)
-
-			message = await ctx.send(embed=embed)
-
-			for emoji in numbers[:len(options)]:
-				await message.add_reaction(emoji)
-
-			self.polls.append((message.channel.id, message.id))
-
-			self.bot.scheduler.add_job(self.complete_poll, "date", run_date=datetime.now()+timedelta(seconds=hours),
-									   args=[message.channel.id, message.id])
-
-	@command(name="giveaway")
-	@has_permissions(manage_guild=True)
-	async def create_giveaway(self, ctx, mins: int, *, description: str):
-		embed = Embed(title="Giveaway",
-					  description=description,
-					  colour=ctx.author.colour,
-					  timestamp=datetime.utcnow())
-
-		fields = [("End time", f"{datetime.utcnow()+timedelta(seconds=mins*60)} UTC", False)]
-
-		for name, value, inline in fields:
-			embed.add_field(name=name, value=value, inline=inline)
-
-		message = await ctx.send(embed=embed)
-		await message.add_reaction("✅")
-
-		self.giveaways.append((message.channel.id, message.id))
-
-		self.bot.scheduler.add_job(self.complete_giveaway, "date", run_date=datetime.now()+timedelta(seconds=mins),
-								   args=[message.channel.id, message.id])
-
-	async def complete_poll(self, channel_id, message_id):
-		message = await self.bot.get_channel(channel_id).fetch_message(message_id)
-
-		most_voted = max(message.reactions, key=lambda r: r.count)
-
-		await message.channel.send(f"The results are in and option {most_voted.emoji} was the most popular with {most_voted.count-1:,} votes!")
-		self.polls.remove((message.channel.id, message.id))
-
-	async def complete_giveaway(self, channel_id, message_id):
-		message = await self.bot.get_channel(channel_id).fetch_message(message_id)
-
-		if len((entrants := [u for u in await message.reactions[0].users().flatten() if not u.bot])) > 0:
-			winner = choice(entrants)
-			await message.channel.send(f"Congratulations {winner.mention} - you won the giveaway!")
-			self.giveaways.remove((message.channel.id, message.id))
-
-		else:
-			await message.channel.send("Giveaway ended - no one entered!")
-			self.giveaways.remove((message.channel.id, message.id))
 
 	@Cog.listener()
 	async def on_raw_reaction_add(self, payload):
