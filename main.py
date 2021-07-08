ba74ddb35739c253abcf6527f8d153d33c1b3a56
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-07-08 01:53:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,54 +26,13 @@ from asyncio import sleep as s
 from asyncio import sleep 
 init(convert=True)
 
-intents = discord.Intents.default()
 
-def get_prefix(client, message): ##first we define get_prefix
-    with open('prefixes.json', 'r') as f: ##we open and read the prefixes.json, assuming it's in the same file
-        prefixes = json.load(f) #load the json as prefixes
-    return prefixes[str(message.guild.id)] #recieve the prefix for the guild id given
-
-client = commands.Bot(command_prefix = get_prefix, intents=intents)
 queue = []
+intents = discord.Intents.default()
 intents.members = True
+client = commands.Bot(command_prefix = ";", intents=intents)
 client.remove_command('help')
 ROLE = "user"
-
-
-@client.event
-async def on_guild_join(guild): #when the bot joins the guild
-    with open('prefixes.json', 'r') as f: #read the prefix.json file
-        prefixes = json.load(f) #load the json file
-
-    prefixes[str(guild.id)] = 'bl!'#default prefix
-
-    with open('prefixes.json', 'w') as f: #write in the prefix.json "message.guild.id": "bl!"
-        json.dump(prefixes, f, indent=4) #the indent is to make everything look a bit neater
-
-@client.event
-async def on_guild_remove(guild): #when the bot is removed from the guild
-    with open('prefixes.json', 'r') as f: #read the file
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id)) #find the guild.id that bot was removed from
-
-    with open('prefixes.json', 'w') as f: #deletes the guild.id as well as its prefix
-        json.dump(prefixes, f, indent=4)
-
-@client.command(pass_context=True)
-@commands.has_permissions(administrator=True) #ensure that only administrators can use this command
-async def changeprefix(ctx, prefix): #command: bl!changeprefix ...
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json', 'w') as f: #writes the new prefix into the .json
-        json.dump(prefixes, f, indent=4)
-
-    await ctx.send(f'Prefix changed to: {prefix}') #confirms the prefix it's been changed to
-#next step completely optional: changes bot nickname to also have prefix in the nickname
-    name=f'{prefix}BotBot'
 
 
 
