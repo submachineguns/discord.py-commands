a698c0138ad7b03b32a5c07e6517607872e4c729
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-07-08 01:48:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,15 +26,16 @@ from asyncio import sleep as s
 from asyncio import sleep 
 init(convert=True)
 
+intents = discord.Intents.default()
+
 def get_prefix(client, message): ##first we define get_prefix
     with open('prefixes.json', 'r') as f: ##we open and read the prefixes.json, assuming it's in the same file
         prefixes = json.load(f) #load the json as prefixes
     return prefixes[str(message.guild.id)] #recieve the prefix for the guild id given
 
-queue = []
-intents = discord.Intents.default()
-intents.members = True
 client = commands.Bot(command_prefix = get_prefix, intents=intents)
+queue = []
+intents.members = True
 client.remove_command('help')
 ROLE = "user"
 
