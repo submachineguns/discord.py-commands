8e86d14bf22bb55ba24b4d3b31734466ef715589
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-07-08 00:35:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -633,26 +633,7 @@ async def on_message(msg):
             await client.process_commands(msg)
 
 
-client.sniped_messages = {}
 
-@client.event
-async def on_message_delete(message):
-    client.sniped_messages[message.guild.id] = (message.content, message.author, message.channel.name, message.created_at)
-
-@client.command()
-async def snipe(ctx):
-    try:
-        contents, author, channel_name, time = client.sniped_messages[ctx.guild.id]
-        
-    except:
-        await ctx.channel.send("Couldn't find a message to snipe!")
-        return
-
-    embed = discord.Embed(description=contents, color = 0xF2684A, timestamp=time)
-    embed.set_author(name=f"{author.name}#{author.discriminator}", icon_url=author.avatar_url)
-    embed.set_footer(text=f"Deleted in : #{channel_name}")
-
-    await ctx.channel.send(embed=embed)
 
 
     
@@ -870,6 +851,26 @@ for cog in cogs:
     client.load_extension("music")
 
 
+snipe_message_author = {}
+snipe_message_content = {}
+ 
+@client.event
+async def on_message_delete(message):
+     snipe_message_author[message.channel.id] = message.author
+     snipe_message_content[message.channel.id] = message.content
+     await sleep(60)
+     del snipe_message_author[message.channel.id]
+     del snipe_message_content[message.channel.id]
+ 
+@client.command()
+async def snipe(ctx):
+    channel = ctx.channel 
+    try:
+        snipeEmbed = discord.Embed(title=f"#{channel.name}", description = snipe_message_content[channel.id])
+        snipeEmbed.set_footer(text=f"Deleted by {snipe_message_author[channel.id]}")
+        await ctx.send(embed = snipeEmbed)
+    except:
+        await ctx.send(f"There are no deleted messages in #{channel.name}")
 
 @client.event
 async def on_message_delete(message):
