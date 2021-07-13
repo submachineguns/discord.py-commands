e1b86d029448a6aa6360432a2a5053c1246d0fb1
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-07-13 17:12:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -933,7 +933,29 @@ async def unjail(ctx, member: discord.Member= None, *, reason='No reason was spe
     await member.send(embed=unretard)
 
 
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_guild_permissions(manage_channels=True)
+    @commands.bot_has_guild_permissions(manage_channels=True)
+    async def lockdown(self, ctx, channel: discord.TextChannel=None):
+        channel = channel or ctx.channel
 
+        if ctx.guild.default_role not in channel.overwrites:
+            overwrites = {
+            ctx.guild.default_role: discord.PermissionOverwrite(send_messages=False)
+            }
+            await channel.edit(overwrites=overwrites)
+            await ctx.send(f"I have put *{channel.name}* on lockdown.")
+        elif channel.overwrites[ctx.guild.default_role].send_messages == True or channel.overwrites[ctx.guild.default_role].send_messages == None:
+            overwrites = channel.overwrites[ctx.guild.default_role]
+            overwrites.send_messages = False
+            await channel.set_permissions(ctx.guild.default_role, overwrite=overwrites)
+            await ctx.send(f"I have put *{channel.name}* on lockdown.")
+        else:
+            overwrites = channel.overwrites[ctx.guild.default_role]
+            overwrites.send_messages = True
+            await channel.set_permissions(ctx.guild.default_role, overwrite=overwrites)
+            await ctx.send(f"I have removed *{channel.name}* from lockdown.")
 
 
 
