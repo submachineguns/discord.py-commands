314ca04ca2da925c852d7aa8695bba07ce58389f
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-07-18 01:33:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ intents.members = True
 client = commands.Bot(command_prefix = ";", intents=intents)
 client.remove_command('help')
 ROLE = "user"
+api_key = 'a90a2e9106e4cc839a1327a5fd8fea00'
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
 
 
@@ -826,8 +828,37 @@ cogs = ["music"]
 for cog in cogs:
     client.load_extension("music")
 
+#weather
 
-
+@client.command()
+async def weather(ctx, *, city: str):
+    city_name = city
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    channel = ctx.message.channel	
+    if x["cod"] != "404":
+        async with channel.typing():	
+            y = x["main"]
+            current_temperature = y["temp"]
+            current_temperature_celsiuis = str(round(current_temperature - 273.15))
+            current_pressure = y["pressure"]
+            current_humidity = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"]	
+            weather_description = z[0]["description"]
+            embed = discord.Embed(title=f"Weather in {city_name}",
+                              color=ctx.guild.me.top_role.color,
+                              timestamp=ctx.message.created_at,)
+            embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
+            embed.add_field(name="Temperature(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
+            embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
+            embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
+            embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            await channel.send(embed=embed)
+    else:
+        await channel.send("City not found.")
 
 @client.command()
 async def snipe(ctx):
