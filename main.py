034117c8492f19c635f22ca3ad1306ddf863ae95
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-07-18 00:20:35 -0400

    .

--- main.py
+++ main.py
@@ -45,70 +45,6 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name=f"world simulator")) # This changes the bots 'activity'
 
 
-@client.event
-async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-
-    await update_data(users, member)
-
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-
-
-@client.event
-async def on_message(message):
-    if message.author.bot == False:
-        with open('users.json', 'r') as f:
-            users = json.load(f)
-
-        await update_data(users, message.author)
-        await add_experience(users, message.author, 5)
-        await level_up(users, message.author, message)
-
-        with open('users.json', 'w') as f:
-            json.dump(users, f)
-
-    await client.process_commands(message)
-
-
-async def update_data(users, user):
-    if not f'{user.id}' in users:
-        users[f'{user.id}'] = {}
-        users[f'{user.id}']['experience'] = 0
-        users[f'{user.id}']['level'] = 1
-
-
-async def add_experience(users, user, exp):
-    users[f'{user.id}']['experience'] += exp
-
-
-async def level_up(users, user, message):
-    with open('levels.json', 'r') as g:
-        levels = json.load(g)
-    experience = users[f'{user.id}']['experience']
-    lvl_start = users[f'{user.id}']['level']
-    lvl_end = int(experience ** (1 / 4))
-    if lvl_start < lvl_end:
-        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
-        users[f'{user.id}']['level'] = lvl_end
-
-@client.command()
-async def level(ctx, member: discord.Member = None):
-    if not member:
-        id = ctx.message.author.id
-        with open('users.json', 'r') as f:
-            users = json.load(f)
-        lvl = users[str(id)]['level']
-        await ctx.send(f'You are at level {lvl}!')
-    else:
-        id = member.id
-        with open('users.json', 'r') as f:
-            users = json.load(f)
-        lvl = users[str(id)]['level']
-        await ctx.send(f'{member} is at level {lvl}!')
-
-#help
 
 @client.group(invoke_without_command=True)
 async def help(ctx):
@@ -122,7 +58,6 @@ async def help(ctx):
 
     await ctx.send(embed = em)
 
-#anti-ad
 
 @client.event
 async def on_message(ctx):
@@ -132,18 +67,9 @@ async def on_message(ctx):
        await ctx.channel.send(embed=emb)
        await client.process_commands(ctx)
 
-#audit logs
 
-async def save_audit_logs(guild):
-     with open(f'audit_logs_{guild.name}', 'w+') as f:
-          async for entry in guild.audit_logs(limit=100):
-               f.write('{0.user} did {0.action} to {0.target}'.format(entry))
 
-@client.command
-async def logs(ctx, message):
-         await save_audit_logs(message.channel.guild)
 
-#helpj
 
 @help.command()
 async def jail(ctx):
