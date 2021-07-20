173c43ade90b48ceaa4cb3c0085fb07bf10e8aae
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-07-19 20:19:22 -0400

    .

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ async def eth(ctx):
 @commands.has_permissions(ban_members=True)
 async def restart(ctx: commands.Context):
 	try:
-		await bot.close()
+		await client.close()
 	except:
 		pass 
 	finally:
@@ -814,7 +814,100 @@ async def on_member_remove(member):
     print(f'{member} has left a server!')
 
 
+#giveaway
 
+@client.command()
+@commands.has_permissions(manage_roles=True)
+async def giveaway(ctx):
+    # Giveaway command requires the user to have a "Giveaway Host" role to function properly
+
+    # Stores the questions that the bot will ask the user to answer in the channel that the command was made
+    # Stores the answers for those questions in a different list
+    giveaway_questions = ['Which channel will I host the giveaway in?', 'What is the prize?', 'How long should the giveaway run for (in seconds)?',]
+    giveaway_answers = []
+
+    # Checking to be sure the author is the one who answered and in which channel
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel
+    
+    # Askes the questions from the giveaway_questions list 1 by 1
+    # Times out if the host doesn't answer within 30 seconds
+    for question in giveaway_questions:
+        await ctx.send(question)
+        try:
+            message = await client.wait_for('message', timeout= 30.0, check= check)
+        except asyncio.TimeoutError:
+            await ctx.send('You didn\'t answer in time.  Please try again and be sure to send your answer within 30 seconds of the question.')
+            return
+        else:
+            giveaway_answers.append(message.content)
+
+    # Grabbing the channel id from the giveaway_questions list and formatting is properly
+    # Displays an exception message if the host fails to mention the channel correctly
+    try:
+        c_id = int(giveaway_answers[0][2:-1])
+    except:
+        await ctx.send(f'You failed to mention the channel correctly.  Please do it like this: {ctx.channel.mention}')
+        return
+    
+    # Storing the variables needed to run the rest of the commands
+    channel = client.get_channel(c_id)
+    prize = str(giveaway_answers[1])
+    time = int(giveaway_answers[2])
+
+    # Sends a message to let the host know that the giveaway was started properly
+    await ctx.send(f'The giveaway for {prize} will begin shortly.\nPlease direct your attention to {channel.mention}, this giveaway will end in {time} seconds.')
+
+    # Giveaway embed message
+    give = discord.Embed(color = 0x2ecc71)
+    give.set_author(name = f'Giveaway')
+    give.add_field(name= f'{ctx.author.name} is giving away: {prize}', value = f'React with 🎉 to enter!\n Ends in {round(time/60, 2)} minutes!', inline = False)
+    end = datetime.datetime.estnow() + datetime.timedelta(seconds = time)
+    give.set_footer(text = f'Giveaway ends at {end} EST')
+    my_message = await channel.send(embed = give)
+    
+    # Reacts to the message
+    await my_message.add_reaction("🎉")
+    await asyncio.sleep(time)
+
+    new_message = await channel.fetch_message(my_message.id)
+
+    # Picks a winner
+    users = await new_message.reactions[0].users().flatten()
+    users.pop(users.index(client.user))
+    winner = random.choice(users)
+
+    # Announces the winner
+    winning_announcement = discord.Embed(color = 0xff2424)
+    winning_announcement.set_author(name = f'Giveaway has ended', icon_url= 'https://i.imgur.com/DDric14.png')
+    winning_announcement.add_field(name = f'🎉 Prize: {prize}', value = f'🥳 **Winner**: {winner.mention}\n 🎫 **Number of Entrants**: {len(users)}', inline = False)
+    winning_announcement.set_footer(text = 'Thanks for entering!')
+    await channel.send(embed = winning_announcement)
+
+#reroll
+
+@client.command()
+@commands.has_permissions(manage_roles=True)
+async def reroll(ctx, channel: discord.TextChannel, id_ : int):
+    # Reroll command requires the user to have a "Giveaway Host" role to function properly
+    try:
+        new_message = await channel.fetch_message(id_)
+    except:
+        await ctx.send("Invalid ID")
+        return
+    
+    # Picks a new winner
+    users = await new_message.reactions[0].users().flatten()
+    users.pop(users.index(client.user))
+    winner = random.choice(users)
+
+    # Announces the new winner to the server
+    reroll_announcement = discord.Embed(color = 0xff2424)
+    reroll_announcement.set_author(name = f'The giveaway was re-rolled by the host')
+    reroll_announcement.add_field(name = f'New Winner:', value = f'{winner.mention}', inline = False)
+    await channel.send(embed = reroll_announcement)
+
+#afk
 
 @client.command()
 async def afk(ctx, reason=None):
@@ -839,10 +932,6 @@ cogs = ["music"]
 for cog in cogs:
     client.load_extension("music")
 
-cogs = ["giveaway"]
-
-for cog in cogs:
-    client.load_extension("giveaway")
 
 #weather
 
