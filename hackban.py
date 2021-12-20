75da2362e0afb04eb944550628133131770a9ef3
submachineguns <73202450+submachineguns@users.noreply.github.com>
2021-12-20 11:50:32 -0500

    Create hackban.py

---
+++ hackban.py
@@ -0,0 +1,12 @@
+#hackban #not a cog command
+
+@client.command(aliases=['hban'])
+@commands.has_permissions(ban_members=True)
+async def hackban(ctx, user: discord.User):
+    if user in ctx.guild.members:
+        emb = discord.Embed(description=f"<:error:867509993884614666> This user is already in the guild", color = 0xec6a6a) #{emoji name} {emoji id}
+        await ctx.send(embed=emb, mention_author=False)
+
+    else:
+        await ctx.guild.ban(user)
+        await ctx.send(f":thumbsup:")
